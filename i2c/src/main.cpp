55f90430ce415b685b56406946f0c1c82a8dad6f
Edwin Svensson <git@olback.net>
2018-10-29 01:48:56 +0100

    Update main.cpp

--- i2c/src/main.cpp
+++ i2c/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <linux/types.h>
 #include "IRi2c/IRi2c.h"
+// #include <unistd.h>
 
 // using namespace std;
 
@@ -8,13 +9,15 @@ int main(int argc, char *argv[]) {
 
     printf("i2c test thing\n");
 
-    // IRi2c::writeData(0x0a, 0); // Max current (mA)
-    // IRi2c::writeData(0x0b, 1000); // Max effect (mW)
-    // IRi2c::writeData(0x0d, 50); // Max temp (C)
-    IRi2c::writeData(IRi2c::IR_ISET_LED1, 100);
-    IRi2c::writeData(IRi2c::IR_PSET_LED1, 1000);
-    IRi2c::writeData(IRi2c::IR_TSET_LED1, 50);
-    IRi2c::readData(IRi2c::IR_VDD);
+    IRi2c::writeData(IRi2c::IR_ISET_LED3, 20);
+    IRi2c::writeData(IRi2c::IR_PSET_LED3, 1000);
+    IRi2c::writeData(IRi2c::IR_TSET_LED3, 50);
+
+    // while (true) {
+    //     IRi2c::readData(IRi2c::IR_VDD);
+    //     IRi2c::readData(IRi2c::IR_LIGHTSENSOR);
+    //     sleep(1);
+    // }
 
     return 0;
 
