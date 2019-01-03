b18226b2413a298daf4cc3ffcc1353184689e20f
Edwin Svensson <git@olback.net>
2019-01-03 15:37:18 +0100

    update api url

--- scanner/lib/api.py
+++ scanner/lib/api.py
@@ -17,7 +17,7 @@ def getBearer():
         return ''
 
 def login(username, password):
-    r = requests.post('https://api.shopper.ink/v1/auth', json={ 'username': username, 'password': password })
+    r = requests.post('https://api.shoplistic.com/v1/auth', json={ 'username': username, 'password': password })
     if r.status_code == 200:
         f = open(authFile, 'w')
         f.write(simplejson.dumps(r.json(), indent=4))
@@ -31,7 +31,7 @@ def login(username, password):
         return False
 
 def get(barcode):
-    r = requests.get('https://api.shopper.ink/v1/bcds/' + barcode, headers = { 'Authorization': 'Bearer ' + getBearer() })
+    r = requests.get('https://api.shoplistic.com/v1/bcds/' + barcode, headers = { 'Authorization': 'Bearer ' + getBearer() })
     if r.status_code == 200:
         return r.json()
     elif r.status_code == 404:
@@ -43,7 +43,7 @@ def get(barcode):
 
 def add(item):
     r = requests.post(
-        'https://api.shopper.ink/v1/shoppinglist',
+        'https://api.shoplistic.com/v1/shoppinglist',
         json = { 'barcode': item['barcode'], 'display_name': item['display_name'], 'manufacturer': item['manufacturer'], 'amount': 1 },
         headers = { 'Authorization': 'Bearer ' + getBearer() }
         )
